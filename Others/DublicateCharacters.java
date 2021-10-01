5b49686f33303a79711b388c2a75f294feb32635
Niko <77497070+Silhouetteless@users.noreply.github.com>
2021-10-01 12:26:58 +0000

    Added DublicateCharacters.java

---
+++ Others/DublicateCharacters.java
@@ -0,0 +1,27 @@
+public class DuplicateCharacters {
+
+public static void main(String[] args) {
+
+  String sentence = "How many duplicates are there?";
+  System.out.println(sentence);
+
+ String characters = ""; //stores every character that we come accross
+
+  String duplicates = "";
+
+  for(int i = 0; i < sentence.length(); i++) {
+    String current = Character.toString(sentence.charAt(i));
+    if(characters.contains(current)) {
+      if(!duplicates.contains(current)) {
+      duplicates += current + ",";
+      }
+    }
+   characters += current; //adding characters 
+   
+
+  }
+
+  System.out.println("duplicates: " + duplicates);
+}
+
+}
