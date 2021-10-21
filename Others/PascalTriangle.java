b4ae440dceb547871d2176c86e7b9e0f9fd7341b
Niko <77497070+Silhouetteless@users.noreply.github.com>
2021-10-21 22:17:12 +0000

    Editted Pascal Triangle.java

--- Others/PascalTriangle.java
+++ Others/PascalTriangle.java
@@ -11,7 +11,10 @@ public class PascalTriangle {
 	return factorial;
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
     //nCr formula:   n ! / ( n – r ) ! r ! 
+=======
+>>>>>>> origin/main
 =======
 >>>>>>> origin/main
     static int ncr(int n,int r) {
@@ -30,11 +33,16 @@ public class PascalTriangle {
 
 	System.out.println("Pascal Triangle:");
 <<<<<<< HEAD
+<<<<<<< HEAD
   //outer iteration i from 0 to n times to print the rows
 	for(i = 0; i < rows; i++) {
     //inner iteration for j from 0 to (rows – 1).
 		for(j = 0; j < rows-i; j++){
       //blank space ” “ for left spacing
+=======
+	for(i = 0; i < rows; i++) {
+		for(j = 0; j < rows-i; j++){
+>>>>>>> origin/main
 =======
 	for(i = 0; i < rows; i++) {
 		for(j = 0; j < rows-i; j++){
