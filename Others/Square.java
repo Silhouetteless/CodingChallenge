c3745d2395eac21f735e6f040ed3366424faf59b
Niko <77497070+Silhouetteless@users.noreply.github.com>
2021-11-13 03:22:26 +0000

    Added Square.java

---
+++ Others/Square.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Square {
+    
+  public static void main(String[] args)
+    {
+             
+      Scanner scan = new Scanner(System.in);
+
+      System.out.println("Enter number of rows & columns: ");
+      int n = scan.nextInt();
+
+      System.out.print("Enter a symbol: ");
+      char c = scan.next().charAt(0);
+    
+      for(int i = 0; i < n; i++) {
+      
+        for(int j = 0; j < n; j++) {
+          System.out.print(c + " ");
+        } 
+        System.out.println();
+      }             
+    
+    scan.close();
+                
+  }
+}
