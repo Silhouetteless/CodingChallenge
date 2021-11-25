e65d0d0a3581959bd0e2784d204a1b5dbb19e497
Niko <77497070+Silhouetteless@users.noreply.github.com>
2021-11-25 06:10:13 +0000

    Added BinarySearch(not complete)

---
+++ Others/BinarySearch.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+ 
+class BinarySearch {
+
+  public static void main(String args[]) {
+ 
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter number of items: ");
+    int n = scan.nextInt(); 
+    int[] array = new int[n];
+ 
+    System.out.println("Enter " + n + " numbers: ");
+ 
+    for (int i = 0; i < n; i++) {
+      array[i] = scan.nextInt();
+    }
+      
+ 
+    System.out.println("Enter value to find: ");
+    int search = scan.nextInt();
+    
+    int first = 0;
+    int last = n - 1;
+    int middle = (first + last) / 2;
+ 
+    while(first <= last) {
+
+        if (array[middle] < search) {
+          first = middle + 1; 
+        }   else if (array[middle] == search) {
+                System.out.println(search + " found at location " + (middle + 1) + ".");
+                break;
+            } else {
+                  last = middle - 1;
+                  middle = (first + last)/2;
+              }
+          
+    }
+
+    if ( first > last ) {
+      System.out.println(search + " is not in the list.\n");
+    }
+      
+  }
+}
