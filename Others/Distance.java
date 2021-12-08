10f9c2ba7cab42e9dabd978e54d08dfaf5b18e29
Niko <77497070+Silhouetteless@users.noreply.github.com>
2021-12-08 11:24:15 +0000

    Added Distance.java

---
+++ Others/Distance.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Distance {
+
+	public static void main(String args[]) {
+ 
+      double speed, distance, time;
+
+      System.out.println("Distance calculator");
+            
+      Scanner scan = new Scanner(System.in);
+    
+      System.out.println("Enter speed in kmph: ");
+    
+      speed=sc.nextDouble();	
+    
+      System.out.println("Enter time in hours: ");
+      
+      time = scan.nextDouble();	
+    
+      distance = speed * time;
+      
+      System.out.println("Distance in kilometers: " + distance + "km");
+    
+      System.out.println("Distance in meters: " + distance * 1000+ "m");
+ 
+      scan.close();
+ 
+ 
+	}
+}
