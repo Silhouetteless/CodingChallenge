154e8a31415bc20b1b3cc921baa00125d7ebbdd2
Niko <77497070+Silhouetteless@users.noreply.github.com>
2021-12-11 14:00:28 +0000

    Added ArrayOrder.java

---
+++ Others/ArrayOrder.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ArrayOrder {
+
+    public static void main(String[] args) {
+
+    
+    	
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number of elements you want in the array: ");
+        int elements = scan.nextInt();
+    
+        int num[] = new int[elements];
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < elements; i++) {
+            num[i] = scan.nextInt();
+        }
+       
+        int temp;
+        for (int i = 0; i < elements; i++)  {
+            for (int j = i + 1; j < elements; j++) { 
+                if (num[i] > num[j]) {
+                    temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+
+        System.out.print("Here is your array in ascending order: ");
+        for (int i = 0; i < elements - 1; i++) {
+            System.out.print(num[i] + ", ");
+        }
+        System.out.print(num[elements - 1]);
+
+         scan.close();
+    }
+}
