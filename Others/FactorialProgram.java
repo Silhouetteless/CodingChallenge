a22dc1c42718ba4b77ea386e73cbe8f4401a72e7
Niko <77497070+Silhouetteless@users.noreply.github.com>
2021-12-13 12:32:27 +0000

    Update

--- Others/FactorialProgram.java
+++ Others/FactorialProgram.java
@@ -1,27 +1,27 @@
 public class FactorialProgram{
 
-public static void main(String[] args) {
+  public static void main(String[] args) {
 
-  /*
-  factorial is a number multipied by each of its proceeding numbers:
-  e.g. 5! = 5 * 4 * 3 *2 * 1 =120
-  */
-  System.out.println(factorial(5));
+    /*
+    factorial is a number multipied by each of its proceeding numbers:
+    e.g. 5! = 5 * 4 * 3 *2 * 1 =120
+    */
+    System.out.println(factorial(5));
 
 
 
-}
-
-public static int factorial(int n) {
-
-  if(n == 1) {
-      return 1; //basecase
-  } else {
-      return n * factorial(n - 1);
   }
 
-  
+  public static int factorial(int n) {
 
-}
+    if(n == 1) {
+        return 1; //basecase
+    } else {
+        return n * factorial(n - 1);
+    }
+
+    
+
+  }
 
 }
