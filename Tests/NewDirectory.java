7ca48d68f55c428f9dad87f6ab8c7eca528eedb2
Niko <77497070+Silhouetteless@users.noreply.github.com>
2021-12-16 06:35:25 +0000

    Added NewDirectory.java

---
+++ Tests/NewDirectory.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+class NewDirectory {
+
+  public static void main(String[] args) {
+
+    //creating a new directory inside Tests(parent directory)
+    //remember about the escape character for \
+    File file = new File(/*path*/"Tests\\directory");
+
+    //when the parent directory already exists use mkdir()
+    boolean value = file.mkdir();
+    //or use file.mkdirs(); if you want to create the parent directory as well
+
+    if(value) {
+      System.out.println("The new directory is created.");
+    }
+    else {
+      System.out.println("The directory already exists.");
+    }
+  }
+
+}
