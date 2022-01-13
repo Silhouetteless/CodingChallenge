552fc16c7f1cd638249fdf6234259e0be6230706
Niko <77497070+Silhouetteless@users.noreply.github.com>
2022-01-13 10:56:04 +0000

    Edit

--- Others/SalaryCalc.java
+++ Others/SalaryCalc.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
-public class Square {
+public class SalaryCalc {
     
-  public static void main(String[] args) {
+  public static void main(String[] args)
+    {
              
       Scanner scan = new Scanner(System.in);
 
